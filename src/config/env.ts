dc1ff8fc7159af8fbdeac2034f6e29f082d7ca0f
sancty <sanctifieryawmensah55@gmail.com>
2025-03-14 16:50:10 +0100

    deploy backend#2

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  PORT: process.env.PORT || 5000,
+  PORT: process.env.PORT,
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
   JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1d",
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
 } as const;
 
 // Validation des variables d'environnement requises

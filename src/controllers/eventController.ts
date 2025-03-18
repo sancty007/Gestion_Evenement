cbe2ae0353e81da46632ee343b231d98f499e4cd
sancty <sanctifieryawmensah55@gmail.com>
2025-03-18 17:50:45 +0100

    add manager error

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from "express";
 
 export const getStatus = async (req: Request, res: Response): Promise<void> => {
-  res.json({ status: "OK" });
+  try {
+    res.json({ status: "OK" });
+  } catch (erro) {
+    console.error("Error:", erro);
+    res.status(500).json({ status: "error", message: "Internal server error" });
+  }
 };
 export const sayHello = async (req: Request, res: Response): Promise<void> => {
-  res.json({ status: "hello word " });
+  try {
+    res.json({ status: "hello word " });
+  } catch (erro) {
+    console.error("Error:", erro);
+  }
 };

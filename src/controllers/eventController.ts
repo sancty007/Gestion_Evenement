a82c8eac4fa3f7309f4678175b36995257c30daa
sancty <sanctifieryawmensah55@gmail.com>
2025-03-18 17:12:57 +0100

    delete express5

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 
-export const getStatus = async (_req: Request, res: Response) => {
+export const getStatus = async (
+  _req: Request,
+  res: Response
+): Promise<void> => {
   res.json({ status: "OK" });
 };

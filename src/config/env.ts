a8cd96fb1aa4ace4e965aee9727a3f36c477a999
sancty <sanctifieryawmensah55@gmail.com>
2025-03-18 17:00:29 +0100

    Revert "deploy backend#2"
    This reverts commit dc1ff8fc7159af8fbdeac2034f6e29f082d7ca0f.

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const config = {
-  PORT: process.env.PORT,
+  PORT: process.env.PORT || 5000,
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
   JWT_SECRET: process.env.JWT_SECRET,
-  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1d",
 } as const;
 
 // Validation des variables d'environnement requises
